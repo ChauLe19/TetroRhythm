ee8bad377f08488b3799c604b53e0b11ab4cca00
ChauLe19 <chaule19@vt.edu>
2021-12-14 06:40:23 +0700

    Fix name

--- Utils.h
+++ Utils.h
@@ -7,7 +7,7 @@ using namespace std;
 
 bool mouseInBox(RenderWindow& window, int x, int y, int width, int height);
 bool mouseInCircle(RenderWindow& window, int x, int y, int r);
-void createButton(RenderWindow& window, Text& text, Color textColor, Color boxColor, string textString, int width, int height, int x, int y);
+void createButton(RenderWindow& window, Text& text,  Color textColor, int fontSize, Color boxColor, string textString, int width, int height, int x, int y);
 
 #endif
 
