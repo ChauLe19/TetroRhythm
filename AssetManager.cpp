659be8a3e8ac477d264c0c4f8a41cb3b333e701b
ChauLe19 <chaunble01@gmail.com>
2023-08-10 17:37:10 -0400

    Asset Manager basic implementation

--- AssetManager.cpp
+++ AssetManager.cpp
@@ -1,1 +1,39 @@
 #include "AssetManager.h"
+
+AssetManager::AssetManager()
+{
+}
+
+AssetManager::~AssetManager()
+{
+}
+
+void AssetManager::loadTexture(std::string name, std::string filename)
+{
+	sf::Texture texture;
+
+	if (texture.loadFromFile(filename))
+	{
+		this->textures[name] = texture;
+	}
+}
+
+sf::Texture& AssetManager::getTexture(std::string name)
+{
+	return this->textures.at(name);
+}
+
+void AssetManager::loadFont(std::string name, std::string filename)
+{
+	sf::Font font;
+
+	if (font.loadFromFile(filename))
+	{
+		this->fonts[name] = font;
+	}
+}
+
+sf::Font& AssetManager::getFont(std::string name)
+{
+	return this->fonts.at(name);
+}
--- AssetManager.h
+++ AssetManager.h
@@ -1,5 +1,22 @@
 #pragma once
+#include <string>
+#include <SFML/Graphics.hpp>
 class AssetManager
 {
+private:
+	std::map<std::string, sf::Texture> textures;
+	std::map<std::string, sf::Font> fonts;
+	std::map<std::string, sf::Font> fonts;
+
+public:
+	AssetManager();
+	~AssetManager();
+
+	void loadTexture(std::string name, std::string fileName);
+	sf::Texture& getTexture(std::string name);
+
+	void loadFont(std::string name, std::string fileName);
+	sf::Font& getFont(std::string name);
+
 };
 
