efbbc75265f621e6088b476f9dd256cab2e6b5e8
ChauLe19 <chaunble01@gmail.com>
2023-09-08 22:40:10 -0400

    key control and mouse control consistent with each other

--- GameOptions.cpp
+++ GameOptions.cpp
@@ -122,6 +122,7 @@ void GameOptions::keyEvent(const float & dt, Event event)
 		if (choosingMap)
 		{
 			cursorMap = clamp(cursorMap - 1, 0, (int)maps.size() - 1);
+			mapRenderOffset = -cursorMap * 200;
 		}
 		else
 		{
@@ -132,6 +133,7 @@ void GameOptions::keyEvent(const float & dt, Event event)
 		if (choosingMap)
 		{
 			cursorMap = clamp(cursorMap + 1, 0, (int)maps.size() - 1);
+			mapRenderOffset = -cursorMap * 200;
 		}
 		else
 		{
